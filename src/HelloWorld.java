df8710a31f045090a0bffa22501c12f2898d0dbe
Koogling <rnrudejr1234@naver.com>
2022-10-04 13:47:02 +0900

    짝수 홀수 나누는 프로그램

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,9 +5,21 @@ public class HelloWorld {
         Scanner sc = new Scanner(System.in);
         System.out.print("입력하세요 : ");
         int n = sc.nextInt();
+        System.out.println(calc(n));
+    }
+    public static String calc(int n){
 
-        if(n%2 == 0){
-
+        if(n == 1 || n == -1){
+            return "홀수";
         }
+        if(n % 2 == 0){
+            return "짝수";
+        }
+
+        if(n % 3 == 0){
+            return "홀수";
+        }
+
+        return "잘못입력함";
     }
 }
