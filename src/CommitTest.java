a5fa0c1f0f32af53ab5c9221085b64459ab1a47a
Koogling <rnrudejr1234@naver.com>
2022-10-04 13:41:11 +0900

    잔돈 거스름돈 체크

--- src/CommitTest.java
+++ src/CommitTest.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class CommitTest {
+    static int money;
+    static int[] count = {50000,10000,5000,1000,500,100,50,10};
+    static int[] result;
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner sc = new Scanner(System.in);
+        money = sc.nextInt();
+        result = new int[8];
+
+        calc();
+        print();
+
+    }
+
+    public static void print(){
+        for(int i =0;i<result.length;i++){
+            System.out.println(count[i] + "원은 몇개 : " +result[i]);
+        }
+    }
+
+    public static void math(int d_money,int index){
+        if(money/d_money != 0){
+            result[index] = money/d_money;
+            money = money%d_money;
+        }
+    }
+    public static void calc(){
+        for(int i = 0; i<8;i++){
+            math(count[i],i);
+        }
     }
 }
