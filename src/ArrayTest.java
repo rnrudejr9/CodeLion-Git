0bdbcd195f08b4a88bda17f648c0b43cfabeaa95
Koogling <rnrudejr1234@naver.com>
2022-10-04 13:22:15 +0900

    ArrayTest

---
+++ src/ArrayTest.java
@@ -0,0 +1,12 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        System.out.println("print hello");
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i+1;
+            System.out.println(arr[i]);
+        }
+
+//      sout
+    }
+}
