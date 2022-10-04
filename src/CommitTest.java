47b2a70af5eefc2e1591a46be80aebdb56889618
Koogling <rnrudejr1234@naver.com>
2022-10-04 14:16:43 +0900

    거스름돈 알고리즘

--- src/CommitTest.java
+++ src/CommitTest.java
@@ -5,15 +5,19 @@ public class CommitTest {
     static int[] count = {50000,10000,5000,1000,500,100,50,10};
     static int[] result;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        money = sc.nextInt();
-        result = new int[8];
 
+        scan();
         calc();
         print();
 
     }
 
+    
+    public static void scan(){
+        Scanner sc = new Scanner(System.in);
+        money = sc.nextInt();
+        result = new int[8];
+    }
     public static void print(){
         for(int i =0;i<result.length;i++){
             System.out.println(count[i] + "원은 몇개 : " +result[i]);
