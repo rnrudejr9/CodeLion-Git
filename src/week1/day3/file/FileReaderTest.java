cf0ec500b991980174791b9e221c3ded8c59aec9
Koogling <rnrudejr1234@naver.com>
2022-10-06 15:11:12 +0900

    현재 디렉토리 목록 출력

--- src/week1/day3/file/FileReaderTest.java
+++ src/week1/day3/file/FileReaderTest.java
@@ -54,6 +54,12 @@ public class FileReaderTest {
             readFile.createANewFile();
         }
 
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        for(File file : files){
+            System.out.println(file);
+        }
+
 
     }
 }
