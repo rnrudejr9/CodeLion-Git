3e7e9f89166696de4bf63533bf337914ab1ae4b1
Koogling <rnrudejr1234@naver.com>
2022-10-06 13:17:56 +0900

    isAlphabet 반영

--- src/week1/day3/MapPractice2.java
+++ src/week1/day3/MapPractice2.java
@@ -3,24 +3,30 @@ package week1.day3;
 import java.util.HashMap;
 
 public class MapPractice2 {
+    public static boolean isAlphabet(char c){
+        if(c>= 65 && c<= 90 || c>=97 && c<=122){
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         String repo = "aCCbcde:::###!!cccc hellodofodfoasdcs";
                 //a, b, c, d, e , f...
         HashMap<Character, Integer> alphabetCnt = new HashMap<>();
         for(int i = 0 ;i<repo.length();i++){
 
-
             char c = repo.charAt(i);
-            if(c>=65 && c<=90){
-                c += 97-65;
-            }
 
-            if(c >= 97 && c <= 122) {
+            if (isAlphabet(c) == true) {
+                if (c >= 65 && c <= 90) {
+                    c += 97 - 65;
+                }
                 if (alphabetCnt.get(c) == null) {
                     alphabetCnt.put(c, 1);
                 } else {
                     alphabetCnt.put(c, alphabetCnt.get(c) + 1);
                 }
+
             }
 
 
