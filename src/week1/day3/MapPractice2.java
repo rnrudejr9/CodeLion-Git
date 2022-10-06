98f333556b511d3f59c24b8e40949dd59104dbbb
Koogling <rnrudejr1234@naver.com>
2022-10-06 11:38:24 +0900

    대소문자 알파벳 구별 추가

--- src/week1/day3/MapPractice2.java
+++ src/week1/day3/MapPractice2.java
@@ -4,15 +4,23 @@ import java.util.HashMap;
 
 public class MapPractice2 {
     public static void main(String[] args) {
-        String repo = "abcdecccc";
+        String repo = "aCCbcde:::###!!cccc hellodofodfoasdcs";
                 //a, b, c, d, e , f...
         HashMap<Character, Integer> alphabetCnt = new HashMap<>();
         for(int i = 0 ;i<repo.length();i++){
+
+
             char c = repo.charAt(i);
-            if(alphabetCnt.get(c) == null){
-                alphabetCnt.put(c,1);
-            }else{
-                alphabetCnt.put(c,alphabetCnt.get(c)+1);
+            if(c>=65 && c<=90){
+                c += 97-65;
+            }
+
+            if(c >= 97 && c <= 122) {
+                if (alphabetCnt.get(c) == null) {
+                    alphabetCnt.put(c, 1);
+                } else {
+                    alphabetCnt.put(c, alphabetCnt.get(c) + 1);
+                }
             }
 
 
