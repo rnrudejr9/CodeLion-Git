72023eca25d8eb324455968248e4bf04cbb77980
Koogling <rnrudejr1234@naver.com>
2022-10-07 11:06:06 +0900

    NewBufferedReader 클래스 분리

--- src/week1/day4/PopulationStatistics.java
+++ src/week1/day4/PopulationStatistics.java
@@ -7,38 +7,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-
-class NewBufferedReader {
-    private String adress;
-
-    public NewBufferedReader(String adress) {
-        this.adress = adress;
-    }
-    public void readByLine() {
-        try (BufferedReader br = Files.newBufferedReader(
-                Paths.get(adress), StandardCharsets.UTF_8)) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public String readByData(){
-        String line;
-        try(BufferedReader br = Files.newBufferedReader(
-                Paths.get(adress), StandardCharsets.UTF_8
-        )) {
-            line = br.readLine();
-        } catch (IOException e ){
-            throw new RuntimeException(e);
-        }
-        return line;
-    }
-}
-
 public class PopulationStatistics {
     public static void main(String[] args){
         String adress = "C:\\Users\\\\KOO\\\\Downloads\\\\2021_인구관련연간자료_20220927_66125.csv";
