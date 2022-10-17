ae889e30fb8976fe96602b7f3c57fe22b78e84d7
Koogling <rnrudejr1234@naver.com>
2022-10-17 09:22:42 +0900

    n개형의 직각삼각형 별찍기

--- src/week3/day1/TrianglePrint.java
+++ src/week3/day1/TrianglePrint.java
@@ -2,9 +2,11 @@ package week3.day1;
 
 public class TrianglePrint {
     public static void main(String[] args) {
-        final int size =5;
-        for(int i= 0;i<5;i++){
-            for(int j = 0 ;j<i;j++){
+        printStar(9);
+    }
+    public static void printStar(int n){
+        for(int i = 0 ;i<n;i++){
+            for(int j = n-i;j<n;j++){
                 System.out.print("*");
             }
             System.out.println();
