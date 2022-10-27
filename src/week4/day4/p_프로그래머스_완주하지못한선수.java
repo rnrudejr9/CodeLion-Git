b57d9afd64d0f03466ac6356d5f66f78bdcecd4f
Koogling <rnrudejr1234@naver.com>
2022-10-27 10:12:35 +0900

    하지못한선수 로직추가

--- src/week4/day4/p_프로그래머스_완주하지못한선수.java
+++ src/week4/day4/p_프로그래머스_완주하지못한선수.java
@@ -5,6 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
+//
+//로직설명
+//map에 참여자들의 명단을 입력받음
+//만약 참여자 명단안에 중복되는 값이 있다면 그 value 값을 ++ 함
+//map에 완주자들의 명단을 입력받음
+//입력받은 완주자 명단에 value 값을 -- 함
+// 따라서 map의 value가 1인값을 갖는 key가 정답이 됨
+//
+//마지막으로 map의 key를 돌면서 0이 아닌 값을 출력함
+
+
 
 public class p_프로그래머스_완주하지못한선수 {
     public static void main(String[] args) throws IOException {
