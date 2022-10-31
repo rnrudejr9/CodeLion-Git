df12c9df1c09d72d7ef9969aabb6a77457883b27
Koogling <rnrudejr1234@naver.com>
2022-10-31 10:10:38 +0900

    Update p_프로그래머스_모의고사.java

--- src/week5/day1/p_프로그래머스_모의고사.java
+++ src/week5/day1/p_프로그래머스_모의고사.java
@@ -3,14 +3,14 @@ package week5.day1;
 
 // 어떤 자료구조, 알고리즘을 사용할 지
 // +@ 알고리즘을 구현해서
+// 3개 변수를 비교하는 방법 ?
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class p_프로그래머스_모의고사 {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] a = {1,3,2,4,2};
+        int[] a = {1,3,2,4,5};
         s.solution(a);
     }
 }
@@ -18,7 +18,7 @@ public class p_프로그래머스_모의고사 {
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {};
+        int[] answer = new int[3];
         int[] a = new int[10001];
         int[] b = new int[10001];
         int[] c = new int[10001];
@@ -26,12 +26,9 @@ class Solution {
         int[] std = {2,1,2,3,2,4,2,5};
         int[] std2 = {3,3,1,1,2,2,4,4,5,5};
         for(int i = 0 ;i<10001;i++){
-            a[i] = std[i%5];
+            a[i] = std3[i%5];
             b[i] = std[i%8];
             c[i] = std2[i%10];
-            System.out.println("a"+a[i]);
-            System.out.println("b"+b[i]);
-            System.out.println("c"+c[i]);
         }
         int acnt= 0;
         int bcnt = 0;
@@ -48,9 +45,7 @@ class Solution {
             }
         }
 
-        int res = Math.max(acnt,bcnt) > Math.max(acnt,ccnt) ? 
-
-
         return answer;
     }
+
 }
