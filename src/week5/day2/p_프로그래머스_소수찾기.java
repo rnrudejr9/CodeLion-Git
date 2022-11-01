350a21638bfe017f6d4129b3a5c62de642f2c42d
Koogling <rnrudejr1234@naver.com>
2022-11-01 09:33:09 +0900

    소수찾기_1번으로 풀기

--- src/week5/day2/p_프로그래머스_소수찾기.java
+++ src/week5/day2/p_프로그래머스_소수찾기.java
@@ -22,13 +22,24 @@ class Solution1{
         int answer = 0;
         int prime = 2;
         int cnt = 0;
-        while(prime != n){
-            if(n % prime != 0){
+        while(prime != n+1){
+            if(isPrime(prime)){
                 cnt++;
             }
             prime++;
         }
         return cnt;
+    }
+    public boolean isPrime(int n){
+        int prime = 2;
+        while(n != prime){
+            if(n % prime == 0){
+                return false;
+            }
+            prime++;
+        }
+        return true;
+
     }
 }
 
