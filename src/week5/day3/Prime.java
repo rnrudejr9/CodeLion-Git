9139f44e56b50a868d5c4364f8c5afeb69e7f552
Koogling <rnrudejr1234@naver.com>
2022-11-02 10:02:31 +0900

    Update Prime.java

--- src/week5/day3/Prime.java
+++ src/week5/day3/Prime.java
@@ -4,18 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Prime {
+    static ArrayList<Integer> list;
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+        list = new ArrayList<>();
         for(int i = 2;i<51;i++){
             list.add(i);
         }
         int index = 1;
-        for(int i = 0; i<list.size();i++){
-            if(list.get(i) % 2 == 0 && list.get(i) > 2){
+        isRemove(2);
+        isRemove(3);
+        isRemove(5);
+        isRemove(7);
+
+        System.out.println(Arrays.toString(list.toArray()));
+    }
+    public static void isRemove(int n){
+        for(int i =0;i<list.size();i++){
+            if(list.get(i) % n == 0 && list.get(i) > n){
                 list.remove(i);
             }
         }
-
-        System.out.println(Arrays.toString(list.toArray()));
     }
 }
