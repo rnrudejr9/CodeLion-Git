06cf292d4ba000526f0546be636a2286589207f8
Koogling <rnrudejr1234@naver.com>
2022-11-03 10:11:50 +0900

    에라토스테네스의 체_배열로구하기

---
+++ src/week5/day4/PrimeArr.java
@@ -0,0 +1,14 @@
+package week5.day4;
+
+import java.util.Arrays;
+
+public class PrimeArr {
+    public static void main(String[] args) {
+        int N = 50; // N = 50
+        int[] nums = new int[N-1]; // N-1개만큼 2~50까지 총 49개
+        boolean[] checks = new boolean[nums.length]; // nums만큼 checks생성
+        Arrays.fill(checks, true); // checks를 true로 초기화 cuz 기본값 false
+        for (int i = 0; i < nums.length; i++) nums[i] = i + 2; // 2~50까지 nums에 채우기
+        System.out.println(Arrays.toString(nums));
+    }
+}
