932dc391edf51f9ec19de11861ceaaf4f53c30d0
Koogling <rnrudejr1234@naver.com>
2022-11-03 09:08:49 +0900

    에라토스테네스의 체를 이용한 소수구하기 50까지

--- src/week5/day3/Prime.java
+++ src/week5/day3/Prime.java
@@ -12,14 +12,21 @@ public class Prime {
         for(int i = 2;i<51;i++){
             list.add(i);
         }
+        arr[0] = true;
+        arr[1] = true;
         int index = 1;
         isRemove(2);
         isRemove(3);
         isRemove(5);
         isRemove(7);
 
-        System.out.println(Arrays.toString(list.toArray()));
-        System.out.println(Arrays.toString(arr));
+        int cnt = 0;
+        for(boolean x : arr){
+            if(x == false){
+                System.out.println(cnt);
+            }
+            cnt++;
+        }
     }
     public static void isRemove(int n){
         for(int i =0;i<list.size();i++){
