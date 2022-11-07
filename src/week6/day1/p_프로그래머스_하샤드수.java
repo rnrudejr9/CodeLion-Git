6145ce57441feb7511f137fadfe02e72499b98e5
Koogling <rnrudejr1234@naver.com>
2022-11-07 09:30:50 +0900

    하샤드수 문자열, 나눗셈으로 풀기

--- src/week6/day1/p_프로그래머스_하샤드수.java
+++ src/week6/day1/p_프로그래머스_하샤드수.java
@@ -2,7 +2,7 @@ package week6.day1;
 
 public class p_프로그래머스_하샤드수 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution2 solution = new Solution2();
         System.out.println(solution.solution(10));
         System.out.println(solution.solution(12));
         System.out.println(solution.solution(11));
@@ -10,10 +10,8 @@ public class p_프로그래머스_하샤드수 {
     }
 }
 
-
 //자릿수의 합
 //나누어떨어지는지 체크
-
 class Solution {
     public boolean solution(int x) {
         boolean answer = false;
@@ -22,6 +20,23 @@ class Solution {
         int temp = 0;
         for(int i=0;i<str.length;i++){
             temp += Integer.parseInt(str[i]);
+        }
+
+        if(x % temp == 0){
+            return answer = true;
+        }
+        return answer;
+    }
+}
+
+class Solution2 {
+    public boolean solution(int x) {
+        boolean answer = false;
+        int temp= 0;
+        int m = x;
+        while (m > 0) {
+            temp += m % 10;
+            m = m / 10;
         }
 
         if(x % temp == 0){
