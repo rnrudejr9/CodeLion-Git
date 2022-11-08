c2b846a4ee0f399e8f2628f1b8e974f5d07f78c9
Koogling <rnrudejr1234@naver.com>
2022-11-08 10:22:01 +0900

    이분탐색 update

--- src/week6/day2/BinaraySearch.java
+++ src/week6/day2/BinaraySearch.java
@@ -1,26 +1,37 @@
 package week6.day2;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BinaraySearch {
     static int cnt;
 
     public static void main(String[] args) {
         BinaraySearch b = new BinaraySearch();
-        int[] arr= new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
-        Arrays.sort(arr);
-
-        b.search(arr.length-1,0,arr,12);
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int find = sc.nextInt();
+        int[] arr = new int[n];
+        sc.nextLine();
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+//        Arrays.sort(arr); 오름차순 정렬됨
+        b.search(arr.length-1,0,arr,find);
     }
     public void search(int high, int low, int[] arr,int target){
-
-
         cnt++;
-        System.out.println("cnt " + cnt);
-        System.out.println("high " + high);
-        System.out.println("low " + low);
-        System.out.println("----------------");
         int temp = (high+low)/2;
+
+        if(target == arr[temp]){
+            System.out.println(temp+1);
+            return;
+        }
+
+        if(cnt > arr.length){
+            System.out.println(-1);
+            return;
+        }
 
         if(arr[temp] > target){
             high = temp -1;
