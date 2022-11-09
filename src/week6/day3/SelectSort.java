255cb4d6733fe108ae2b20e73c66bdb3a35d2790
Koogling <rnrudejr1234@naver.com>
2022-11-09 10:03:05 +0900

    Update SelectSort.java

--- src/week6/day3/SelectSort.java
+++ src/week6/day3/SelectSort.java
@@ -4,21 +4,21 @@ import java.util.Arrays;
 
 public class SelectSort {
     public static void main(String[] args) {
-        int[] arr= new int[]{2,7,9,4,10,223,111,23,3,39};
+        int[] arr= new int[]{2,7,9,4,10,223,111,23,3,39,2,2,7,7};
 
         for(int i = 0; i<arr.length;i++){
-            decreaseSort(arr,i);
+            decendingSort(arr,i);
+            System.out.println(i+1 +"회차");
+            System.out.println(Arrays.toString(arr));
         }
-
-        System.out.println(Arrays.toString(arr));
-
         for(int i = 0; i<arr.length;i++){
-            increaseSort(arr,i);
+            ascendingSort(arr,i);
+            System.out.println(i+1 +"회차");
+            System.out.println(Arrays.toString(arr));
         }
-        System.out.println(Arrays.toString(arr));
     }
 
-    public static void decreaseSort(int[] arr,int idx){
+    public static void decendingSort(int[] arr,int idx){
         int max = arr[idx];
         int findIdx = idx;
         for(int i = idx;i<arr.length;i++){
@@ -33,7 +33,7 @@ public class SelectSort {
             arr[findIdx] = temp;
         }
     }
-    public static void increaseSort(int[] arr,int idx){
+    public static void ascendingSort(int[] arr,int idx){
         int max = arr[idx];
         int findIdx = idx;
         for(int i = idx;i<arr.length;i++){
