d3d86ba41534115d6e340a8bf6b50c76fc998d68
Koogling <rnrudejr1234@naver.com>
2022-11-09 09:20:17 +0900

    선택정렬 구현, 오름차순 내림차순

--- src/week6/day3/SelectSort.java
+++ src/week6/day3/SelectSort.java
@@ -7,17 +7,37 @@ public class SelectSort {
         int[] arr= new int[]{2,7,9,4,10,223,111,23,3,39};
 
         for(int i = 0; i<arr.length;i++){
-            sort(arr,i);
+            decreaseSort(arr,i);
         }
 
         System.out.println(Arrays.toString(arr));
+
+        for(int i = 0; i<arr.length;i++){
+            increaseSort(arr,i);
+        }
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void sort(int[] arr,int idx){
+    public static void decreaseSort(int[] arr,int idx){
         int max = arr[idx];
         int findIdx = idx;
         for(int i = idx;i<arr.length;i++){
             if(max < arr[i]){
+                max = arr[i];
+                findIdx = i;
+            }
+        }
+        if(max != arr[idx]){
+            int temp = arr[idx];
+            arr[idx] = max;
+            arr[findIdx] = temp;
+        }
+    }
+    public static void increaseSort(int[] arr,int idx){
+        int max = arr[idx];
+        int findIdx = idx;
+        for(int i = idx;i<arr.length;i++){
+            if(max > arr[i]){
                 max = arr[i];
                 findIdx = i;
             }
