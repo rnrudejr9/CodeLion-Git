53ff2f471fe3b7ae29579799bb729066467f678b
Koogling <rnrudejr1234@naver.com>
2022-11-10 09:08:29 +0900

    week6.day4 같은숫자는 싫어 진행중

---
+++ src/week6/day4/Solution.java
@@ -0,0 +1,18 @@
+package week6.day4;
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        HashMap<Integer, Integer> map =new HashMap<>();
+        int index = 0;
+        for(int i =0;i<arr.length-1;i++){
+            
+        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
---
+++ src/week6/day4/p_프로그래머스_같은숫자는싫어.java
@@ -0,0 +1,5 @@
+package week6.day4;
+
+public class p_프로그래머스_같은숫자는싫어 {
+
+}
