43d2035b3f3dc517ee042872bc48bedd0b703305
Koogling <rnrudejr1234@naver.com>
2022-11-10 09:16:54 +0900

    프로그래머스 숫자는 싫어 해결

--- src/week6/day4/Solution.java
+++ src/week6/day4/Solution.java
@@ -5,13 +5,21 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         int[] answer = {};
-        HashMap<Integer, Integer> map =new HashMap<>();
+        Stack<Integer> st = new Stack<>();
         int index = 0;
         for(int i =0;i<arr.length-1;i++){
-            
+            if(st.isEmpty()){
+                st.push(arr[i]);
+            }else if(st.peek() != arr[i]) {
+                st.push(arr[i]);
+            }
+        }
+        answer = new int[st.size()];
+
+        for(Integer a : st){
+            answer[index++] = a;
         }
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
 
         return answer;
     }
--- src/week6/day4/p_프로그래머스_같은숫자는싫어.java
+++ src/week6/day4/p_프로그래머스_같은숫자는싫어.java
@@ -1,5 +1,11 @@
 package week6.day4;
 
-public class p_프로그래머스_같은숫자는싫어 {
+import java.util.Arrays;
 
+public class p_프로그래머스_같은숫자는싫어 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = new int[]{1,1,3,3,0,1,1};
+        System.out.println(Arrays.toString(s.solution(arr)));
+    }
 }
