3c67f7d4b40f56f0f4d6f0846655d6452b402073
Koogling <rnrudejr1234@naver.com>
2022-11-11 09:34:27 +0900

    십진수를 이진수 변환 + 비밀지도

--- src/week6/day5/Solution.java
+++ src/week6/day5/Solution.java
@@ -3,18 +3,62 @@ package week6.day5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 //
 //배열에 관해 2진수로 표현하고
 //표현된 2진수는 서로서로 or 연산 실행 -> 새로운 문자열 저장
 
 public class Solution {
+    public String DextoBin(int n){
+        String s = "";
+        while(n/2 != 0){
+            int mod = n%2;
+            n = n / 2;
+            s = s + mod;
+        }
+        s = s + (n%2);
+        System.out.println("binary " + s);
+        return s;
+    }
+
+    public String isSmall(String s,int n){
+        if(s.length() < n){
+            int p = n - s.length();
+            for(int i=0 ;i<p;i++) {
+                s = "0" + s;
+            }
+        }
+        return s;
+    }
     public String[] solution(int n, int[] arr1, int[] arr2) throws IOException {
         String[] answer = {};
 
         for(int i =0;i<n;i++){
+            int a1 = arr1[i];
+            int b1 = arr2[i];
+            DextoBin(a1);
+            DextoBin(b1);
+
             String s =Integer.toBinaryString(arr1[i]);
             String s2 = Integer.toBinaryString(arr2[i]);
+
+            s = isSmall(s,n);
+            s2 = isSmall(s2,n);
+
+            String[] temp = new String[n];
+            for(int j=0;j<s.length();j++){
+                char ca = s.charAt(j);
+                char cb = s2.charAt(j);
+                temp[j] = "";
+                if(ca == '0' && cb == '0'){
+                    temp[j] += '0';
+                }else if(ca == '1' || cb == '1') {
+                    temp[j] += '#';
+                }
+
+            }
+            System.out.println(Arrays.toString(temp));
         }
 
 
--- src/week6/day5/p_프로그래머스_비밀지도.java
+++ src/week6/day5/p_프로그래머스_비밀지도.java
@@ -1,4 +1,13 @@
 package week6.day5;
 
+import java.io.IOException;
+
 public class p_프로그래머스_비밀지도 {
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+        int n = 5;
+        int[] arr1 = new int[]{9,20,28,18,11};
+        int[] arr2 = new int[]{30,1,21,17,28};
+        s.solution(n,arr1,arr2);
+    }
 }
