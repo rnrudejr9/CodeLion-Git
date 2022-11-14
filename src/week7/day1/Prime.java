0bb7c19c03db877523e1781b902f9a3ead576127
Koogling <rnrudejr1234@naver.com>
2022-11-14 10:23:49 +0900

    에라토스테네스의 체 구현

--- src/week7/day1/Prime.java
+++ src/week7/day1/Prime.java
@@ -6,29 +6,31 @@ public class Prime {
     static int[] arr;
 
     public static void main(String[] args) {
-        arr= new int[50];
+        int n = 50;
+        arr= new int[n];
         for(int i =0;i<arr.length;i++){
             arr[i] = i+2;
         }
-
-        isPrime(5);
+        int cnt = 0;
+        isPrime();
         System.out.println(Arrays.toString(arr));
+        for(int x : arr){
+            if(x == 0) {
+                continue;
+            }else{
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
     }
-    public static void isPrime(int n){
+    public static void isPrime(){
+        int[] primeArr = new int[]{2,3,5,7};
         for(int i=0;i<arr.length;i++){
-            if(arr[i] % 2 == 0 && arr[i] != 2){
-                arr[i] = 0;
+            for(int j = 0;j<primeArr.length;j++) {
+                if (arr[i] % primeArr[j] == 0 && arr[i] != primeArr[j]) {
+                    arr[i] = 0;
+                }
             }
-            if(arr[i] %3 == 0 && arr[i] != 3){
-                arr[i] = 0;
-            }
-            if(arr[i] %5 == 0 && arr[i] != 5){
-                arr[i] = 0;
-            }
-            if(arr[i] %7 == 0 && arr[i] != 7){
-                arr[i] = 0;
-            }
-
         }
     }
 }
