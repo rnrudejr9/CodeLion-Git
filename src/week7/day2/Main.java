05bf3c887adcde56b2f24404c007d09c499c5787
Koogling <rnrudejr1234@naver.com>
2022-11-15 09:35:26 +0900

    퀵소트 구현중

--- src/week7/day2/Main.java
+++ src/week7/day2/Main.java
@@ -2,6 +2,8 @@ package week7.day2;
 
 public class Main {
     public static void main(String[] args) {
-
+        Integer[] arr = {20, 18, 5, 19, 6, 25, 40, 50};
+        QuickSort q= new QuickSort();
+        q.quickSort(arr);
     }
 }
--- src/week7/day2/QuickSort.java
+++ src/week7/day2/QuickSort.java
@@ -1,27 +1,30 @@
 package week7.day2;
 
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 // 배열의 반을 기준점으로 삼는다. size/2 인덱스를 피봇으로
 public class QuickSort {
     static Deque<Integer> deque;
-
     public QuickSort() {
         deque = new LinkedList<>();
     }
-
-    public static void quickSort(int[] arr){
+    public void quickSort(Integer[] arr){
         int pivot = arr[arr.length/2];
-        deque.add(pivot);
+        ArrayList<Integer> small = new ArrayList<>();
+        ArrayList<Integer> big = new ArrayList<>();
         for(int x : arr){
             if(pivot < x){
-                deque.addLast(x);
+                big.add(x);
             }else if(pivot > x){
-                deque.addFirst(x);
+                small.add(x);
             }
+        }
+        for(int x : small){
+            System.out.println(x);
+        }
+        for(int x : big){
+            System.out.println(x);
         }
     }
 }
