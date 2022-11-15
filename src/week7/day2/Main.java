f217eadf048220375ccd27700b09a8aabb48cbcb
Koogling <rnrudejr1234@naver.com>
2022-11-15 10:35:09 +0900

    퀵정렬 merge, sort메소드

--- src/week7/day2/Main.java
+++ src/week7/day2/Main.java
@@ -6,8 +6,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>();
-        for(int i=0;i<10;i++){
-            arr.add(i);
+        int[] arrays = new int[]{20,18,5,19,5,25,40,50};
+
+        for(int i=0;i<arrays.length;i++){
+            arr.add(arrays[i]);
         }
         QuickSort q= new QuickSort();
         q.quickSort(arr);
--- src/week7/day2/QuickSort.java
+++ src/week7/day2/QuickSort.java
@@ -1,27 +1,34 @@
 package week7.day2;
 
 
+
 import java.util.*;
 
 // 배열의 반을 기준점으로 삼는다. size/2 인덱스를 피봇으로
 public class QuickSort {
-    public List<Integer> sort(List<Integer> arr){
-
+    public List<Integer> merge(List<Integer> small,List<Integer> mid,List<Integer> big){
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(small);
+        answer.addAll(mid);
+        answer.addAll(big);
+        return answer;
     }
     public List<Integer> quickSort(List<Integer> arr){
         // 기준값을 뽑는 로직
         int pivot = arr.get(arr.size()/2);
         List<Integer> small = new ArrayList<>();
         List<Integer> big = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
         // 기준값  기준 나누는 로직
         for(int x : arr){
             if(pivot < x){
                 big.add(x);
             }else if(pivot > x){
                 small.add(x);
+            }else{
+                mid.add(x);
             }
         }
-
-        return sort(small) + pivot +sort(big);
+        return merge(quickSort(small),quickSort(mid),quickSort(big));
     }
 }
