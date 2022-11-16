71e6375b5f63c1c75d3f1f785cff37caedc2a8f2
Koogling <rnrudejr1234@naver.com>
2022-11-16 09:29:03 +0900

    week7.day3 퀵정렬 배열로 구현하기 (진행중)

---
+++ src/week7/day3/QuickSortByArray.java
@@ -0,0 +1,27 @@
+package week7.day3;
+
+public class QuickSortByArray {
+    public void quickSort(int[] arr){
+        int pivot = arr[arr.length/2];
+        int leftIdx = 0;
+        int rightIdx = arr.length-1;
+
+        if(pivot > arr[leftIdx]){
+            leftIdx+=1;
+        }else if(pivot < arr[rightIdx]){
+            rightIdx+=1;
+        }
+
+        if(arr[leftIdx] <= rightIdx){
+            swap(arr,leftIdx,rightIdx);
+            leftIdx+=1;
+            rightIdx+=1;
+        }
+
+    }
+    public void swap(int[] arr,int left,int right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+}
