d28f624bcf32e26268f14ac9c5703dacc81d2889
Koogling <rnrudejr1234@naver.com>
2022-11-18 10:00:50 +0900

    시저암호 진행중

--- src/week7/day5/Main.java
+++ src/week7/day5/Main.java
@@ -3,6 +3,6 @@ package week7.day5;
 public class Main {
     public static void main(String[] args) {
         p_프로그래머스_시저암호 p = new p_프로그래머스_시저암호();
-        p.solution("abc",3);
+        p.solution("A Z a z",3);
     }
 }
--- src/week7/day5/p_프로그래머스_시저암호.java
+++ src/week7/day5/p_프로그래머스_시저암호.java
@@ -4,18 +4,39 @@ public class p_프로그래머스_시저암호 {
     public String solution(String s, int n) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
-        for(int i = 0 ;i<s.length();i++){
-            if(s.charAt(i) == ' '){
-                continue;
-            }
-            if(s.charAt(i) == 'z') {
-                sb.append('a');
-            }else if(s.charAt(i) == 'Z'){
-                sb.append('A');
-            }
-            sb.append((char)(s.charAt(i) + 1));
+        int size = 25;
+        char[] c = new char[size];
+        char[] C = new char[size];
+        for(int i =0;i<size;i++){
+            C[i] = (char)(65 + i);
+        }
+        for(int i = 0;i<size;i++){
+            c[i] = (char)(97+i);
         }
 
+        for(int i = 0 ;i<s.length();i++){
+            if(s.charAt(i) >= 97 && s.charAt(i) <= 122){
+                int temp = s.charAt(i) + n;
+                if( temp > 'z'){
+                    temp = temp - 'z';
+                    sb.append(c[temp]);
+                }else{
+                    temp = temp - 'a';
+                    sb.append(c[temp]);
+                }
+            }else if(s.charAt(i) >= 'A' || s.charAt(i) <= 'Z'){
+                int temp = s.charAt(i) + n;
+                if( temp > 'Z'){
+                    temp = temp - 'Z';
+                    sb.append(c[temp]);
+                }else{
+                    temp = temp - 'A';
+                    sb.append(c[temp]);
+                }
+            }else{
+                sb.append(' ');
+            }
+        }
         System.out.println(sb.toString());
 
         return answer;
