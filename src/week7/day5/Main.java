03a20eb3b89c431a9e2336b425b4191b3a68eed5
Koogling <rnrudejr1234@naver.com>
2022-11-18 10:14:58 +0900

    시저암호 해결_조건문

--- src/week7/day5/Main.java
+++ src/week7/day5/Main.java
@@ -3,6 +3,9 @@ package week7.day5;
 public class Main {
     public static void main(String[] args) {
         p_프로그래머스_시저암호 p = new p_프로그래머스_시저암호();
-        p.solution("A Z a z",3);
+        p.solution("A Z a z", 3);
+        p.solution("z", 1);
+        p.solution("a B z", 4);
+        p.solution("      ", 5);
     }
 }
--- src/week7/day5/p_프로그래머스_시저암호.java
+++ src/week7/day5/p_프로그래머스_시저암호.java
@@ -4,39 +4,31 @@ public class p_프로그래머스_시저암호 {
     public String solution(String s, int n) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
-        int size = 25;
-        char[] c = new char[size];
-        char[] C = new char[size];
-        for(int i =0;i<size;i++){
-            C[i] = (char)(65 + i);
-        }
-        for(int i = 0;i<size;i++){
-            c[i] = (char)(97+i);
-        }
 
         for(int i = 0 ;i<s.length();i++){
-            if(s.charAt(i) >= 97 && s.charAt(i) <= 122){
-                int temp = s.charAt(i) + n;
-                if( temp > 'z'){
-                    temp = temp - 'z';
-                    sb.append(c[temp]);
-                }else{
-                    temp = temp - 'a';
-                    sb.append(c[temp]);
-                }
-            }else if(s.charAt(i) >= 'A' || s.charAt(i) <= 'Z'){
-                int temp = s.charAt(i) + n;
-                if( temp > 'Z'){
-                    temp = temp - 'Z';
-                    sb.append(c[temp]);
-                }else{
-                    temp = temp - 'A';
-                    sb.append(c[temp]);
-                }
-            }else{
+            if(s.charAt(i) == ' '){
                 sb.append(' ');
+                continue;
+            }
+            if(s.charAt(i) >= 97 && s.charAt(i) <= 122){
+                char ch = (char) (s.charAt(i) + n);
+                if(ch > 122){
+                    ch = (char) (ch - 'z' + 'a' -1);
+                    sb.append(ch);
+                }else{
+                    sb.append(ch);
+                }
+            }else if(s.charAt(i) >= 'A' || s.charAt(i) <= 'Z') {
+                char ch = (char) (s.charAt(i) + n);
+                if (ch > 90) {
+                    ch = (char) (ch - 'Z' + 'A' - 1);
+                    sb.append(ch);
+                } else {
+                    sb.append(ch);
+                }
             }
         }
+
         System.out.println(sb.toString());
 
         return answer;
