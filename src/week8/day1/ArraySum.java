c34958cb600bb7b6ebe19ee4943cff81348405de
Koogling <rnrudejr1234@naver.com>
2022-11-22 09:44:23 +0900

    array 모든값 더하기

--- src/week8/day1/ArraySum.java
+++ src/week8/day1/ArraySum.java
@@ -3,8 +3,14 @@ package week8.day1;
 public class ArraySum {
     public static void main(String[] args) {
         int[] arr= new int[]{7,1,3,9};
+        System.out.println(sumArr(arr,0,0));
     }
-    public static void sumArr(int length){
+    public static int sumArr(int[]arr, int cnt,int sum){
+        if(cnt == arr.length){
+            return sum;
+        }
+        sum += arr[cnt];
+        return sumArr(arr,cnt+1,sum);
 
     }
 }
