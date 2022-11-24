5cd29e524615bc5b0760efbe436f5fb9ae1dcf67
Koogling <rnrudejr1234@naver.com>
2022-11-24 09:16:58 +0900

    피보나치수열 구현

---
+++ src/week8/day2/fibo2.java
@@ -0,0 +1,7 @@
+package week8.day2;
+
+public class fibo2 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/week8/day3/fibo.java
+++ src/week8/day3/fibo.java
@@ -1,25 +1,29 @@
-package week8.day2;
+package week8.day3;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class fibo {
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        fibonacci(n);
+        int n =Integer.parseInt(bufferedReader.readLine());
+        arr = new int[n+1];
+        System.out.println(fibonacci(n));
     }
 
     public static int fibonacci(int n) {
 
-        if(n == 1){
-            return 1;
-        } if(n==2){
-            return 1;
+        if(arr[n] != 0){
+            return arr[n];
         }
 
-
-        return fibonacci(n-1) + fibonacci(n-2);
+        if(n == 1){
+            return arr[n] = 1;
+        } if(n==2){
+            return arr[n] = 2;
+        }
+        return arr[n] = fibonacci(n-1) + fibonacci(n-2);
     }
 }
