2e2bed8f86986b76547d8c8506f17eed46c91de4
pi koo <rnrudejr1234@naver.com>
2022-12-05 23:46:29 +0900

    Commit

---
+++ src/algorithm/p_예산.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+public class p_예산 {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        return answer;
+    }
+}
+
---
+++ src/week10/day1/p_heap.java
@@ -0,0 +1,8 @@
+package week10.day1;
+
+public class p_heap {
+    public static void main(String[] args) {
+        int[] arr = new int[]{6,1,2,3,5,4};
+
+    }
+}
